c622bfd72a7aa93ebacd269c80fc0439aa68bca7
Ubuntu2 <c.avogadro@ufl.edu>
2016-12-03 19:35:33 -0500

    added Longest common subset code and apperas to work, debugging is next

--- z_algo.cpp
+++ z_algo.cpp
@@ -5,6 +5,52 @@
 using namespace std;
 
 
+
+string printlcs(string s1,string s2)
+{
+    int m,n;
+    m=s1.size(),n=s2.size();
+    vector< vector< pair<int,string> > >dp(m+1,vector< pair<int,string> > (n+1,make_pair(0,"")));
+    pair<int,string> pp1;
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(s1[i-1]==s2[j-1])
+            {
+                pp1=dp[i-1][j-1];
+                int num=pp1.first+1;
+                string str=pp1.second+s1[i-1];
+                //Add spacing to sequence only if it's not the last element
+                if(j == n )
+                {
+                
+                    dp[i][j]=make_pair(num,str);
+                }
+                else
+                {
+                
+                    dp[i][j]=make_pair(num,str+" ");
+                
+                }
+            }
+            else
+            {
+                if(dp[i-1][j]>=dp[i][j-1])
+                {
+                    pp1=dp[i-1][j];
+                }
+                else
+                {
+                    pp1=dp[i][j-1];
+                }
+                dp[i][j]=pp1;
+            }
+        }
+    }
+    return dp[m][n].second;
+}
+
 class ZAlgorithm 
 {
 
@@ -87,13 +133,46 @@ class ZAlgorithm
 
 int main()
 {
-    string text = "bbbbb";
-    string pattern = "bbb";
+    string hint;
+    int n;
+    string line;
+    string column = "";
+    string sequence_row="";
+    string sequence_col="";
+    vector<string> matrix;
 
     ZAlgorithm *zAlgorithm = new ZAlgorithm();
 
-    int result = zAlgorithm->match_pattern(text,pattern);
+    cin>>hint;
+    cin>>n;
 
-    cout<<result<<endl;
+    for(int i = 0; i<n; i++)
+    {
+        cin>>line;
+        matrix.push_back(line);
+    
+    }
+    //Perform Z algorithm on the rows of the matrix
+
+    for(string s:matrix)
+    {
+        sequence_row+= to_string(zAlgorithm->match_pattern(s,hint));
+    
+    }
+
+    for(int i = 0; i<n;i++)
+    {
+        column = "";
+        for(int j = 0;j < n;j++)
+        {
+            column+= matrix[j][i];
+        
+        }
+        sequence_col+= to_string(zAlgorithm->match_pattern(column,hint));
+    
+    }
+    cout<<printlcs(sequence_row,sequence_col);
+
+
     return 0;
 }
